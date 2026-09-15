6f317c2749bb95bf267668df5b316308d91cf4d7
HAHOON <dlxogns8997@gmail.com>
2026-09-16 01:20:33 +0900

    Add DinnerWheel

--- Assets/GameGraphicsLab/Script/TriggerParticle.cs
+++ Assets/GameGraphicsLab/Script/TriggerParticle.cs
@@ -1,34 +1,77 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class TriggerParticle : MonoBehaviour
 {
+
     [SerializeField]
-    private ParticleSystem _particleOnTrigger;
+    private ParticleSystem _particlePrefab;
     [SerializeField]
     private Vector3 _particleOffset;
     
     private ParticleSystem _thisPs;
     private List<ParticleSystem.Particle> _enterParticles = new List<ParticleSystem.Particle>();
+    private ObjectPool<ParticleSystem> _psPool;
 
     private void Awake()
     {
+        _psPool = new(CreateParticle, OnGetParticle, OnReleaseParticle, OnDestroyParticle, false, 5, 10);
         _thisPs = GetComponent<ParticleSystem>();
+    }
+
+    private ParticleSystem CreateParticle()
+    {
+        return Instantiate(_particlePrefab);
+    }
+
+    private void OnGetParticle(ParticleSystem ps)
+    {
+        ps.gameObject.SetActive(true);
+    }
+
+    private void OnReleaseParticle(ParticleSystem ps)
+    {
+        ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        ps.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyParticle(ParticleSystem ps)
+    {
+        if (ps != null)
+        {
+            Destroy(ps.gameObject);
+        }
     }
 
     private void OnParticleTrigger()
     {
-        if(!_particleOnTrigger)
+        if(!_particlePrefab)
         {
             return;
         }
         int count = _thisPs.GetTriggerParticles(ParticleSystemTriggerEventType.Inside, _enterParticles);
         if(count > 0)
         {
-            _particleOnTrigger.transform.position = _enterParticles[0].position + _particleOffset;
-            _particleOnTrigger.Clear(true);
-            _particleOnTrigger.Play(true);
+            OnPlayParticle(_enterParticles[0].position);
+        }
+    }
+
+    private async void OnPlayParticle(Vector3 position)
+    {
+        ParticleSystem ps = _psPool.Get();
+        ps.transform.position = position + _particleOffset;
+        ps.Play(true);
+        Debug.Log("Call");
+        while (ps != null && ps.isPlaying)
+        {
+            await Task.Yield();
         }
 
+        if (ps != null)
+        {
+            _psPool.Release(ps);
+        }
     }
 }
--- Assets/GameGraphicsLab/Script/VFXController.cs
+++ Assets/GameGraphicsLab/Script/VFXController.cs
@@ -5,11 +5,41 @@ using UnityEngine.VFX;
 public class VFXController : MonoBehaviour
 {
     [SerializeField]
+    private float _baseSpinTorque = 10f;
+    [SerializeField]
+    private Rigidbody _dinnerWheelRigid;
+    [SerializeField]
     private VisualEffect _vfx;
+    [SerializeField]
+    private ParticleSystem _fireworkPs;
 
+    private bool _onClick = false;
+    private bool _hasStartedSpinning = false;
     private void Awake()
     {
         _vfx.Reinit();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_onClick)
+        {
+            return;
+        }
+
+        float angularSpeed = _dinnerWheelRigid.angularVelocity.sqrMagnitude;
+        if (!_hasStartedSpinning)
+        {
+            _hasStartedSpinning = angularSpeed > 0.1f;
+            return;
+        }
+
+        if (angularSpeed <= 0.1f)
+        {
+            _fireworkPs.Play();
+            _onClick = false;
+            _hasStartedSpinning = false;
+        }
     }
 
     public void OnFire(InputValue inputValue)
@@ -19,8 +49,12 @@ public class VFXController : MonoBehaviour
             return;
         }
 
-       // _vfx.Reinit();
+        _vfx.Reinit();
+        float randomSpeed = Random.Range(_baseSpinTorque - 7f, _baseSpinTorque);
+        _dinnerWheelRigid.AddTorque(_dinnerWheelRigid.transform.forward * randomSpeed, ForceMode.Impulse);
         _vfx.Play();
         Debug.Log("Fire");
+        _onClick = true;
+        _hasStartedSpinning = false;
     }
 }
